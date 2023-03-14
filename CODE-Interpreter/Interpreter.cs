885f0fe54b304a14b75a6d2c590761555dc27b2c
Kid Omar Costelo <kidomar.costelo@cit.edu>
2023-03-14 21:46:43 +0800

    Create interpreter

---
+++ CODE-Interpreter/Interpreter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CODE_Interpreter
+{
+    /// <summary>
+    /// Kani na class kay serves as the interpreter pipepline, ari e process tanab processes
+    /// </summary>
+    internal class Interpreter
+    {
+        private string _code;
+        private Lexer _lexer;
+        private Parser _parser;
+
+        private  List<string> _errorMessages;
+        public Interpreter(string code)
+        { 
+            _code = code;
+            _lexer = new Lexer(code);
+        }
+
+        public List<string> ErrorMessages { get => _errorMessages; }
+        // temporary name
+        public bool Interpret()
+        {
+            int error;
+            _parser = new Parser(_lexer.Tokenize());
+            error =  _parser.Parse();
+
+            if (error == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                _errorMessages = _parser.ErrorMessages;
+                return false;
+            }
+        }
+    }
+}
